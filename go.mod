f56db26d664b3ea36b62f5dbe39c6f9ae342833c
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2025-06-21 12:03:52 -0700

    x

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/devicemxl/nexusl
 
 go 1.24.3
-require (
-    ./pkg/ds v0.0.0
-    ./pkg/lexer v0.0.0
-)
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 	line := "Hello, world! This is a test.\n Let's see how it works."
-	words := lexer.stepOne(line)
+	words := lx.stepOne(line)
 	for _, word := range words {
 		println(word)
 	}
